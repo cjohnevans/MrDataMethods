836024aa28141c1f40f2aa200a25ef0ddbe2f43c
sapje1 <evansj31@cardiff.ac.uk>
2022-04-13 12:36:19 +0100

    moved scripts to python_mrscripts

--- twix_utils.py
+++ twix_utils.py
@@ -7,11 +7,9 @@ def twix_dump_data(twix_file, start_pos=0, nbytes=4096):
       Dump the first nbytes of twix_file header to twix_dump
       Defaults to reading 4096 bytes.
     '''
-    print(nbytes)
     if nbytes > 20000 or nbytes == -1:
         print('nbytes too large. resetting.')
         nbytes = 20000
-    print(nbytes)
     fid = open(twix_file,'rb')
     # start at start_pos relative to beginning of twix file and read a chunk of the header
     fid.seek(start_pos, os.SEEK_SET)
@@ -20,47 +18,42 @@ def twix_dump_data(twix_file, start_pos=0, nbytes=4096):
     fid.close()
     return(twix_dump)
 
-def twix_strip_junk(twix_tmp):
+def twix_find_header_start(twix_tmp):
     '''
+    twix_find_header_start(b_twix_data)
+      b_twix_data is binary array of twix data (from twix_dump_data)
+      returns the byte of the FIRST location in the array of the actual header data
     '''
     #find the entry 'Config', then count backwards to get to the correct start of the file
     pattern = br'Config'
     matches = re.search(pattern, twix_tmp, re.DOTALL)
     # Config should be at position 8
     new_start_pos = matches.start() - 8
-    twix_out = bytearray(twix_tmp[new_start_pos:])
-    return(twix_out)
+    return(new_start_pos)
 
-def twix_strip_write(input_file, output_file, max_bytes=-1):
+def twix_partial_write(input_file, output_file, start_pos, max_bytes=-1):
     '''
-    twix_strip_junk(twix_file)
+    twix_partial_write(input_file, output_file, start_pos, max_bytes=-1)
         input_file = twix .dat file for input
         output_file = filename for output (will be overwritten if existing)
-        strip rubbish at the start of twix file header by locating identifiable params in header
-        read data from input_file and write to output_file bytewise
     '''
     try:
         open(output_file, 'rb')
-        print("Writing to ", output_file);
+        val = input(str(output_file) + " exists.  Overwrite?")
+        if val not in [ 'y','Y']:
+            return False
     except FileNotFoundError:
-        print(output_file, "exists.  Try a different file name")
-        return False
+        print ('Writing to ', output_file)
         
-
-    orig_header = twix_dump_data(input_file)
-    #find the entry 'Config', then count backwards to get to the correct start of the file
-    pattern = br'Config'
-    matches = re.search(pattern, orig_header, re.DOTALL)
-    # Config should be at position 8
-    new_start_pos = matches.start() - 8
-
     with open(input_file, 'rb') as f_in:
         with open(output_file, 'wb') as f_out:
             byte = b'1'
-            f_in.seek(new_start_pos)
-            while byte:
+            pos=f_in.seek(start_pos)
+            while ( byte and pos < max_bytes):
                 byte = f_in.read(1)
-                f_out.write(byte)
+                pos = f_in.tell()
+                print(str(pos), str(byte))
+                #f_out.write(byte)
 
     return True
 
