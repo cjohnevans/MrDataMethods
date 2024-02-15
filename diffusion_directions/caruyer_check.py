2eacc6b5e8ebadf36167108395557fba170af573
cjohnevans <c.john.evans@gmail.com>
2024-02-15 20:30:37 +0000

    macbook changes

--- diffusion_directions/caruyer_check.py
+++ diffusion_directions/caruyer_check.py
@@ -12,7 +12,10 @@ import os, sys
 sys.path.append('/Users/john/code/MrDataMethods/diffusion_directions')
 import siemensdirclass as dirns
 
+# this takes input from the Caruyer 'multishell' script 
+
+
 ms = dirns.CaruyerDir('multishell')
-ms.read_caruyer('/Users/john/code/temp.txt')
-ms.plotbval(1000, 'test')
+ms.read_caruyer('/Users/john/code/qspace/scripts/01-shells-12.txt')
+ms.plotbval(1000, 'Caruyer 1 shell, 12 dir')
 ms.plotsphere()
