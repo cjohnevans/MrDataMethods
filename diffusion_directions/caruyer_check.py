fb1f9f5da201ed7d93340bf294f09eb2482f7e36
sapje1 <evansj31@cardiff.ac.uk>
2024-02-15 20:32:03 +0000

    wl026 directions changes

--- diffusion_directions/caruyer_check.py
+++ diffusion_directions/caruyer_check.py
@@ -9,10 +9,13 @@ Created on Wed Feb 14 12:03:10 2024
 """
 
 import os, sys
-sys.path.append('/Users/john/code/MrDataMethods/diffusion_directions')
+#sys.path.append('/Users/john/code/MrDataMethods/diffusion_directions')
+sys.path.append('/home/sapje1/code/python_mrobjects/diffusion_directions')
 import siemensdirclass as dirns
 
 ms = dirns.CaruyerDir('multishell')
-ms.read_caruyer('/Users/john/code/temp.txt')
+#ms.read_caruyer('/Users/john/code/temp.txt')
+ms.read_caruyer('/home/sapje1/code/qspace/scripts/03-shells-60-60-60.txt')
+
 ms.plotbval(1000, 'test')
 ms.plotsphere()
